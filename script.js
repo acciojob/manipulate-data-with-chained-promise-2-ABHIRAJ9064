1967eaa37acbfa9ba38b4a0da5e1857484b8ddcc
ABHIRAJ9064 <159516528+ABHIRAJ9064@users.noreply.github.com>
2024-08-12 19:44:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,48 @@
 //your JS code here. If required.
-const express = require('express');
-const path = require('path');
+function manipulateArray(inputArray) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(inputArray);
+        }, 3000);
+    });
+}
 
-const app = express();
+// Function to filter out odd numbers and update the output div after 1 second
+function filterOddNumbers(array) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            const filteredArray = array.filter(num => num % 2 === 0);
+            resolve(filteredArray);
+        }, 1000);
+    });
+}
 
-app.use(express.static(__dirname))
+// Function to multiply even numbers by 2 and update the output div after 2 seconds
+function multiplyEvenNumbers(array) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            const multipliedArray = array.map(num => (num % 2 === 0) ? num * 2 : num);
+            resolve(multipliedArray);
+        }, 2000);
+    });
+}
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/main.html'));
-});
-//your code here
-app.post('/add', (req, res) => {
-  const {a,b} = req.body;
-  res.status(200).send(a+b);
-  // res.sendFile(path.join(__dirname + '/main.html'));
-});
-module.exports = app;
+// Function to update the text of an HTML element with the given ID
+function updateOutputText(elementId, text) {
+    document.getElementById(elementId).innerText = text;
+}
+
+// Initial array
+const inputArray = [1, 2, 3, 4];
+
+// Chain promises to manipulate the array and update the output div
+manipulateArray(inputArray)
+    .then(filterOddNumbers)
+    .then(multiplyEvenNumbers)
+    .then(resultArray => {
+        // Update the text of the HTML element with ID "output"
+        updateOutputText("output", JSON.stringify(resultArray));
+    })
+    .catch(error => {
+        console.error("Error:", error);
+    });
